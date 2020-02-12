0b3f8f54a20e50b321109a8bfc09ce59dc716fca
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2020-02-12 13:10:53 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,8 +13,8 @@ if Rails.env.production?
       region: 'ap-northeast-1'
     }
 
-    config.fog_directory  = 'yudaishimoji42865233'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yudaishimoji42865233'
+    config.fog_directory  = 'community-board5872'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/community-board5872'
   end
   
 else
