2d483140e0dbbc49a5d80b318ecc1d8f8adef6f3
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2020-03-22 11:16:14 +0900

    add BASIC_AUTH

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -7,4 +8,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password ==  ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+  
 end
